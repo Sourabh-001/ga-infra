c8de88ecfa6d94201094b0243d2a7d4740a4d19a
Sourabh-001 <sourabhg002@gmail.com>
2024-02-21 00:18:47 +0530

    start

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
 // Navbar.js
 import React from 'react';
 import "./Navbar.css"
+import logo from '../assets/ga_infra-removebg-preview.png';
 
 function Navbar() {
   return (
-    <nav className="Navbar">
-      <div className="navbar-container">
-        <div className="navbar-logo">
-          <a href="/">Logo</a>
-        </div>
-        <ul className="navbar-menu">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </div>
-    </nav>
+    <nav class="navbar navbar-expand-sm ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="javascript:void(0)"><img src={logo} alt="Logo" height={30} width={50}/></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
   );
 }
 
